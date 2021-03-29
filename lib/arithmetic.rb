dd426f37e467628317068dac211d5defa410639c
Alexis C <alenux1@gmail.com>
2021-03-29 10:06:34 -0400

    Commit started

---
+++ lib/arithmetic.rb
@@ -0,0 +1,13 @@
+module Arithmetic
+  class Operations
+    def initialize
+    end
+    def addition(a=nil,b=nil)
+     raise StandardError.new 'add only integer' unless Integer(a) && Integer(b) 
+     a.to_i + b.to_i
+    end
+    def substraction(a,b)
+      a - b
+    end
+  end
+end
---
+++ spec/aritmetics_operation/arithmeric_spec.rb
@@ -0,0 +1,39 @@
+require "spec_helper"
+require 'arithmetic'
+RSpec.describe 'Arithmetic Operation >' do
+  it 'Instance class' do
+    operation = Arithmetic::Operations.new
+  end
+  context 'addition' do
+    subject { Arithmetic::Operations.new }
+    it 'add two values' do
+      expect(subject.addition(4,5)).to eq(9)
+    end
+    it 'Exception only integer' do
+      expect {subject.addition('a',5)}.to raise_error(ArgumentError)
+    end
+    it 'Convert string to integer and to do addition ' do
+      expect(subject.addition("12","12")).to be(24)
+    end
+  end
+
+  context 'substraction' do
+    subject {Arithmetic::Operations.new}
+
+    it "is there method substraction" do
+      expect(subject).to respond_to(:substraction)
+    end
+
+    it "Try substraction get back number negetive" do
+      expect(subject.substraction(4,5)).to be(-1)
+    end
+    
+    it ' Get subs with number positive' do
+      expect(subject.substraction(9,0)).to be(9)
+    end
+
+    it 'Exception only numbers to add' do
+      expect {subject.substraction('a',5)}.to raise_error(ArgumentError)
+    end
+  end
+end
