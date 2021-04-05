0a5fd94393a5fec932320842de057394a635b8d2
Alexis C <alenux1@gmail.com>
2021-04-05 17:01:04 -0400

    added division, logarithm and expotentation

--- lib/arithmetic.rb
+++ lib/arithmetic.rb
@@ -11,6 +11,15 @@ module Arithmetic
     def multiplication(a=nil, b=nil)
      (a.to_i * b.to_i)  if check_inputs?(a,b)
     end
+    def division(a=nil, b=nil)
+     (a.to_f / b.to_f)  if check_inputs?(a,b)
+    end
+    def expo(a=nil, b=nil)
+      a.to_i ** b.to_i if check_inputs?(a,b) 
+    end
+    def log(a=nil, base=nil)
+      sprintf("%5.1f",Math.log(a,base)).to_f if check_inputs?(a,base)
+    end
 
     private
     def check_inputs?(a,b)
