97d9ded4b944b7237cefc76301a9f08fa5c5fb78
Alexis C <alenux1@gmail.com>
2021-04-21 07:33:08 -0400

    the algorithm work only parentheses, left curly brackets and square brackets

--- lib/arithmetic.rb
+++ lib/arithmetic.rb
@@ -20,6 +20,17 @@ module Arithmetic
     def log(a=nil, base=nil)
       sprintf("%5.1f",Math.log(a,base)).to_f if check_inputs?(a,base)
     end
+    def by_sign(operation)
+      operation = operation.to_s
+      pattern_brackets = /\(([0-9].*?)\)/
+      numbers_inside_sign= []
+      while operation.scan(pattern_brackets).size > 0  do 
+        regex_result = operation[pattern_brackets]
+        result_to_operation = operation[pattern_brackets,1]
+        operation.gsub!(regex_result,eval(result_to_operation).to_s)
+      end
+      operation = eval(operation)
+    end
 
     private
     def check_inputs?(a,b)
