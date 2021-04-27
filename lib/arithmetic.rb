3653e2f05380ba88477f9bd11bfbec9c37ba9399
Alexis C <alenux1@gmail.com>
2021-04-27 08:04:17 -0400

    N² loop to search square and parentheses already

--- lib/arithmetic.rb
+++ lib/arithmetic.rb
@@ -22,12 +22,13 @@ module Arithmetic
     end
     def by_sign(operation)
       operation = operation.to_s
-      pattern_brackets = /\(([0-9].*?)\)/
-      numbers_inside_sign= []
-      while operation.scan(pattern_brackets).size > 0  do 
-        regex_result = operation[pattern_brackets]
-        result_to_operation = operation[pattern_brackets,1]
-        operation.gsub!(regex_result,eval(result_to_operation).to_s)
+      pattern_brackets = [/\(([0-9].*?)\)/,/\[([0-9].*?)\]/]
+      pattern_brackets.each_with_index do |j,i|
+        while operation.scan(j).size > 0  do 
+          regex_result = operation[j]
+          result_to_operation = operation[j,1]
+          operation.gsub!(regex_result,eval(result_to_operation).to_s)
+        end
       end
       operation = eval(operation)
     end
