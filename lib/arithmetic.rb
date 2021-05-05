a9e03cc888b26657849892e5c0b7448eb2930ccc
Alexis C <alenux1@gmail.com>
2021-05-05 10:37:33 -0400

    add conditional to check which regex into array works and so on

--- lib/arithmetic.rb
+++ lib/arithmetic.rb
@@ -15,20 +15,22 @@ module Arithmetic
       sprintf('%5.1f',(a.to_f / b.to_f)).to_f  if check_inputs?(a,b)
     end
     def expo(a=nil, b=nil)
-     sprintf('%5.1f', (a.to_f ** b.to_f)).to_f if check_inputs?(a,b) 
+      sprintf('%5.1f', (a.to_f ** b.to_f)).to_f if check_inputs?(a,b) 
     end
     def log(a=nil, base=nil)
       sprintf("%5.1f",Math.log(a,base)).to_f if check_inputs?(a,base)
     end
     def by_sign(operation)
       operation = operation.to_s
-      pattern_brackets = [/\(([0-9].*?)\)/,/\[([0-9].*?)\]/]
-      pattern_brackets.each_with_index do |j,i|
-        while operation.scan(j).size > 0  do 
-          regex_result = operation[j]
-          result_to_operation = operation[j,1]
-          operation.gsub!(regex_result,eval(result_to_operation).to_s)
-        end
+      pattern_brackets = [/\(([0-9].*?)\)/,/\[([0-9].*?)\]/,/\{([0-9].*?)\}/]
+      i = pattern_brackets[0] =~ operation ? 0 : (pattern_brackets[1]=~operation) ? 1 : 2 
+      while i>=0 && operation.scan(pattern_brackets[i]).size > 0 do 
+        brackets = pattern_brackets[i]
+        regex_result = operation[brackets]
+        result_to_operation = operation[brackets,1]
+        operation.gsub!(regex_result,eval(result_to_operation).to_s)
+        result = pattern_brackets[i] =~ operation
+        i+=1 if result.nil? && !pattern_brackets[i+1].nil?
       end
       operation = eval(operation)
     end
